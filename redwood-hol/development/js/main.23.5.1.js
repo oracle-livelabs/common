2ffe34a02fe091f9385005b4fd18a2edcccb1f53
Kaylien Phan <kaylienvphan@gmail.com>
2025-02-26 14:17:41 -0800

    Update main.23.5.1.js

--- redwood-hol/development/js/main.23.5.1.js
+++ redwood-hol/development/js/main.23.5.1.js
@@ -818,22 +818,22 @@ let main = function () {
         $('.selected div.arrow').click();
     }
     
-    // let triggerTranslation = function () {
-    //     if (window.parent && window.parent.document) {
-    //         let body = window.parent.document.body;
+    let triggerTranslation = function () {
+        if (window.parent && window.parent.document) {
+            let body = window.parent.document.body;
             
-    //         // Add a hidden character to an element that Chrome might watch
-    //         let triggerElement = window.parent.document.getElementById("translation-trigger");
-    //         if (!triggerElement) {
-    //             triggerElement = window.parent.document.createElement("span");
-    //             triggerElement.id = "translation-trigger";
-    //             triggerElement.style.display = "none";
-    //             body.appendChild(triggerElement);
-    //         }
+            // Add a hidden character to an element that Chrome might watch
+            let triggerElement = window.parent.document.getElementById("translation-trigger");
+            if (!triggerElement) {
+                triggerElement = window.parent.document.createElement("span");
+                triggerElement.id = "translation-trigger";
+                triggerElement.style.display = "none";
+                body.appendChild(triggerElement);
+            }
             
-    //         triggerElement.textContent = new Date().getTime(); // Change the text to force re-translation
-    //     }
-    // };
+            triggerElement.textContent = new Date().getTime(); // Change the text to force re-translation
+        }
+    };
     
     /* The following function performs the event that must happen when the lab links in the navigation is clicked */
     let changeTutorial = function (file_name, anchor = "") {
@@ -845,9 +845,10 @@ let main = function () {
         // parent.location.reload();
         // window.parent.document.body.setAttribute("title", Date.now());
         // Safely modify the parent page’s style
-        if (window.parent && window.parent.document) {
-            window.parent.document.body.style.textShadow = "rgba(0, 0, 0, 0.001) 0px 0px 1px";
-        }
+        // if (window.parent && window.parent.document) {
+        //     window.parent.document.body.style.textShadow = "rgba(0, 0, 0, 0.001) 0px 0px 1px";
+        // }
+        triggerTranslation();
     }
 
     /*the following function changes the path of images as per the path of the MD file.
