b3e2f16782d5bde999df076533e755aa7a156688
Kaylien Phan <kaylienvphan@gmail.com>
2025-06-16 15:51:45 -0700

    Update main.23.6.js

--- redwood-hol/development/js/main.23.6.js
+++ redwood-hol/development/js/main.23.6.js
@@ -1072,7 +1072,7 @@ let main = function () {
             let popupContent = `
            <div class="translation-popup-content">
             <h2>How to Translate This Page</h2>
-            <p>You must be on an <strong>livelabs.oracle.com</strong> domain to be able to use translations, not an apexapps.oracle.com domain.</p>
+            <p>You must be on an <strong>livelabs.oracle.com</strong> domain to be able to use <em>translations, not apexapps.oracle.com.</p>
             <p>For the best translation experience, we recommend <strong>Google Chrome</strong>.</p>
 
             <!-- Tabs -->
