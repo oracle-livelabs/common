97b7969cfb135b1cd23edce9b5aa63de8649f9b3
Kaylien Phan <kaylienvphan@gmail.com>
2025-06-18 16:59:27 -0700

    Update main.23.6.js

--- redwood-hol/development/js/main.23.6.js
+++ redwood-hol/development/js/main.23.6.js
@@ -1141,7 +1141,7 @@ let main = function () {
                     referrerpolicy="no-referrer"
                 > in the Edge address<br> bar and select the desired language.
             </li>
-            <li> Click the Translate button.<li>
+            <li> Click the Translate button.</li>
             <li>If you navigate to a new lab and the translation disappears, <br> repeat steps 1 and 2 to re-enable it.</li>
         </ol>
     </div>
