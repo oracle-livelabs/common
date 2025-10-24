7fd8bf8cfb52960a9ad71ce24702b5fd4cd17cc8
Kevin Lazarz <kevin.lazarz@oracle.com>
2025-10-24 13:52:06 +0200

    Update resize-recur.sh

--- scripts/img-resize/resize-recur.sh
+++ scripts/img-resize/resize-recur.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# run this script in the root directopry of a workshop to recursivley resize all images larger than 1mb
+# run this script in the root directopry of a workshop to recursivley resize all larger images to max 1920px
 # you need to have ffmpeg installed. Should work on Linux and Mac
 
-find . -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) -size +1M -print0 |
+find . -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) -print0 |
 while IFS= read -r -d '' f; do
   ext="${f##*.}"
   base="${f%.*}"
@@ -11,10 +11,13 @@ while IFS= read -r -d '' f; do
   tmp="${base}.tmp.${ext_lc}"
 
   echo "Resizing: $f"
-  ffmpeg -loglevel error -nostdin -i "$f" -vf "scale=iw/2:ih/2" -y "$tmp" || {
-    echo "Failed on: $f" >&2
-    continue
-  }
+  ffmpeg -loglevel error -nostdin -i "$f" \
+    -vf "scale='min(iw,1920)':'min(ih,1920)':force_original_aspect_ratio=decrease" \
+    -y "$tmp" || {
+      echo "Failed on: $f" >&2
+      continue
+    }
 
   mv -f "$tmp" "$f"
 done
+
